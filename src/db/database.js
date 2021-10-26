71bd103735c8a108baea0a7971dd48386772290e
Oforduhh <oforduhh@gmail.com>
2021-10-26 17:29:32 +0100

    dotenv bugs

--- src/db/database.js
+++ src/db/database.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
 process.env.NODE_ENV !== "production"
   ? (URL = "mongodb://127.0.0.1:27017/Authentication_system")
   : (URL = process.env.MONGO_URI);
 
+console.log(process.env.MONGO_URI);
 const dbConnection = async () => {
   await mongoose
     .connect(process.env.MONGO_URI, {
