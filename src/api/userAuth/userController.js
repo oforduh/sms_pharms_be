2b4fe219fb12becfb241899486645620b891e522
Oforduhh <oforduhh@gmail.com>
2022-02-06 22:34:12 +0100

    edit delete and change password Api has been created

--- src/api/userAuth/userController.js
+++ src/api/userAuth/userController.js
@@ -1,14 +1,10 @@
 import userModel from "../../model/userModel.js";
 import responses from "../../helper/responses.js";
+import bcrypt from "bcryptjs";
 
 export const handleUserRegistration = async (req, res) => {
   const { email, password, fName, lName, age } = req.body;
   try {
-    // if (!email || !password || !fName || lName)
-    //   return responses.not_found({
-    //     res,
-    //     message: "Insert the required input field",
-    //   });
     const user = new userModel({ email, password, fName, lName, age });
     await user.save();
     const token = await user.generateAuthToken();
@@ -112,6 +108,126 @@ export const getLoggedUserDetails = async (req, res) => {
     return responses.bad_request({
       res,
       message: "Server Error ",
+      e,
+    });
+  }
+};
+
+// Update a user profile
+export const updateUserProfile = async (req, res) => {
+  try {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["fName", "lName", "email", "age"];
+    const isValidOperation = updates.every((update) => {
+      return allowedUpdates.includes(update);
+    });
+
+    console.log(isValidOperation);
+
+    if (!isValidOperation)
+      return responses.bad_request({
+        res,
+        message: "Update Failed",
+      });
+    updates.forEach((update) => {
+      req.user[update] = req.body[update];
+    });
+    await req.user.save();
+    return responses.success({
+      res,
+      data: req.user,
+    });
+  } catch (e) {
+    if (e.kind === "Number") {
+      return res.status(400).send({
+        error: "Age Should be a number",
+      });
+    }
+    if (e.code === 11000) {
+      if (e.keyValue.email) {
+        return res.status(400).send({
+          message: "Email already exist",
+        });
+      }
+    }
+    if (e.name === "ValidationError") {
+      if (e.errors.email) {
+        return res.status(400).send({
+          message: e.errors.email.message,
+        });
+      }
+      if (e.errors.fName) {
+        return res.status(400).send({
+          message: e.errors.fName.message,
+        });
+      }
+      if (e.errors.lName) {
+        return res.status(400).send({
+          message: e.errors.lName.message,
+        });
+      }
+      if (e.errors.age) {
+        return res.status(400).send({
+          message: e.errors.age.message,
+        });
+      }
+    }
+    return responses.bad_request({
+      res,
+      message: "Update Failed",
+      e,
+    });
+  }
+};
+
+// change a logged in user password
+export const change_password = async (request, res) => {
+  const user = request.user;
+
+  try {
+    const { old_password, new_password } = request.body;
+
+    const is_match = await bcrypt.compare(old_password, user.password);
+    const same_password = await bcrypt.compare(new_password, user.password);
+
+    if (!is_match)
+      return responses.bad_request({
+        res,
+        message: "Incorrect Password",
+      });
+
+    if (same_password)
+      return responses.bad_request({
+        res,
+        message: "New Password cannot be the same as the old password",
+      });
+
+    user.password = new_password;
+    await user.save();
+
+    responses.success({
+      res,
+      message: "Password updated successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+// How to delete a user account from the database
+export const handleDeleteProfile = async (req, res) => {
+  try {
+    await req.user.remove();
+    return responses.success({
+      res,
+      message: "This Account has been Deleted",
+      data: req.user,
+    });
+  } catch (e) {
+    return responses.bad_request({
+      res,
+      message: "Unable to delete user ",
       e,
     });
   }
--- src/api/userAuth/userRouter.js
+++ src/api/userAuth/userRouter.js
@@ -6,6 +6,9 @@ import {
   handleUserLogin,
   handleUserLogout,
   getLoggedUserDetails,
+  updateUserProfile,
+  change_password,
+  handleDeleteProfile,
 } from "./userController.js";
 import { authenticateUser } from "../../helper/authenticateUser.js";
 
@@ -13,5 +16,8 @@ router.post("/register", handleUserRegistration);
 router.post("/login", handleUserLogin);
 router.post("/logout", authenticateUser, handleUserLogout);
 router.get("/user/me", authenticateUser, getLoggedUserDetails);
+router.patch("/user/me/edit", authenticateUser, updateUserProfile);
+router.post("/changePassword", authenticateUser, change_password);
+router.delete("/user/me", authenticateUser, handleDeleteProfile);
 
 export default router;
